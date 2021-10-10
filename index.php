c90f20077bd71637489690c3dcc81b09ed3ee615
Sujoy <sujoykarmakar807@gmail.com>
2021-10-10 23:56:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="css/style.css">
 
     <title>OnlineStudy4u | one stop solution for all your placement needs</title>
 </head>
